e943b5ae11a70da01623d047317ab1bbc7e50cd3
robin <xiaoyao_lx@aliyun.com>
2019-04-11 11:27:59 +0800

    配置swagger2 api接口同时添加云库作为基础测试
    云库仅作为基础功能测试，业务数据测试建议在本地数据库中进行测试。

---
+++ src/main/java/com/hw/utils/Swagger2.java
@@ -0,0 +1,50 @@
+package com.hw.utils;/**
+ * Created by robin on 2019/4/11.
+ */
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author: Robin
+ * @create: 2019-04-11 11:03
+ * @description: Swagger2 配置目录
+ **/
+
+@Configuration
+@EnableSwagger2
+public class Swagger2 {
+
+    @Value("${swagger.enable}")
+    private boolean enableSwagger;
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(clientApiInfo())
+                .enable(enableSwagger)
+                .groupName("ALL")
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.hw.web"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo clientApiInfo() {
+        return new ApiInfoBuilder()
+                .title("gameapi")
+                .description("hw-game V1.0")
+                .termsOfServiceUrl("")
+                .contact("我是谁")
+                .version("1.0")
+                .build();
+    }
+}
